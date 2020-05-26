4dff5a7b43f318bc225cbaac6a397aae7890a7c3
Norma <norma.urcadiz.p@gmail.com>
2020-05-26 14:31:39 -0600

    fin de la seccion 7

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Producto } from '../interfaces/producto.interface';
 
+
 @Injectable({
   providedIn: 'root'
 })
@@ -9,6 +10,7 @@ export class ProductosService {
 
   cargando = true;
   productos: any[] = [];
+  productosFiltrado: Producto[] = [];
 
   constructor( private http: HttpClient) { 
       this.cargarProductos();
@@ -16,19 +18,61 @@ export class ProductosService {
   }
 
   private cargarProductos (){
+
+    return new Promise ( (resolve, reject ) => {
+
       this.http.get('https://angular-html-4dd6b.firebaseio.com/productos_id.json')
       .subscribe( (resp: Producto[]) => {
-
-        console.log(resp);
-        this.productos=resp;
-        // this.productos = resp;
+        this.productos = resp;
         this.cargando = false;
-
         setTimeout(() => {
-
         }, 2000);
+        resolve();
 
       });
+
+
+    });
+
+  }
+  getProducto (id: string){
+    return this.http.get(`https://angular-html-4dd6b.firebaseio.com/productos/${id}.json`);
+  }
+
+  buscarProducto (termino: string){
+
+  if (this.productos.length ===0 ){
+    //carg PRODUCTOS
+    this.cargarProductos().then(  () => {
+    //ejecutar despues de tener los productos
+    //aplicar filtro
+    this.filtrarProductos( termino );
+    })
+  }else{
+    //aplicar filtro
+    this.filtrarProductos( termino );
+  }
+
+  //   this.productosFiltrado = this.productos.filter( producto => {
+  //     return true;
+  //   });
+  //   console.log (this.productosFiltrado);
+   }
+
+  private filtrarProductos(termino: string){
+    //console.log (this.productos );
+    this.productosFiltrado = [];
+
+    termino = termino.toLocaleLowerCase();
+
+    this.productos.forEach( prod => {
+
+      const tituloLower = prod.titulo.toLocaleLowerCase();
+
+      if (prod.categoria.indexOf( termino ) >= 0 || tituloLower.indexOf( termino ) >= 0 ){
+        this.productosFiltrado.push ( prod );
+      }
+    });
   }
 
 }
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { InforPaginaService } from '../../services/infor-pagina.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-header',
@@ -8,9 +10,18 @@ import { InforPaginaService } from '../../services/infor-pagina.service';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor(public _servicio: InforPaginaService) { }
+  constructor(public _servicio: InforPaginaService,
+              private router: Router) { }
 
   ngOnInit() {
+  }
+
+  buscarProducto( termino: string){
+    if ( termino.length <1 ){
+      return;
+    }
+    this.router.navigate(['/search', termino]);
+    //console.log(termino);
   }
 
 }
